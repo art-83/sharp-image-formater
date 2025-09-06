efb6cbdd42e5e8f202253751ee82dc075ebaed5d
art-83 <arthurfmedeiros77@gmail.com>
2025-09-06 20:52:50 -0300

    feat: image formated

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import sharp from "sharp";
+import fs from "fs";
 import { ImageRotateService } from "./service/image-rotate.service";
 
 const imageRotateService = new ImageRotateService();
 
 async function teste() {
-    for(let i = 1; i <= 7; i++) {
+    for(let i = 1; i <= fs.readdirSync(__dirname + "/media").length - 1; i++) {
         const image = sharp(__dirname + "/media/" + i + ".jpeg");
         const imageFormated = await imageRotateService.formatImage(image);
         imageFormated.toFile(__dirname + "/media/result/result_" + i + ".jpeg"); 
